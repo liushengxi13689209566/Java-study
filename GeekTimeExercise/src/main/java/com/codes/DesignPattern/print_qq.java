3955392543f2535c8178865262261e0990c4f8e8
senci <liushengxi@sencideMacBook-Pro.local>
2021-05-12 11:59:14 +0800

    添加 guava 库

--- GeekTimeExercise/src/main/java/com/codes/DesignPattern/print_qq.java
+++ GeekTimeExercise/src/main/java/com/codes/DesignPattern/print_qq.java
@@ -1,10 +1,32 @@
 package com.codes.DesignPattern;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class print_qq {
     public static void main(String[] args) {
+
+/*        int a = 23;
+        ImmutableList<Integer> list = ImmutableList.of(a, 12);
+        System.out.println(list);
+        a = 232;
+        System.out.println(list);
+
+        List<String> list02 = Lists.newArrayList("a", "b", "c", "d");
+
+        //1、正常遍历
+        list02.forEach(item -> System.out.println(item));
+        //2、根据条件遍历
+        list02.forEach(item -> {
+            if ("b".equals(item)) {
+                System.out.println(item);
+            }
+        });*/
+
 
         ExecutorService pool = Executors.newSingleThreadExecutor();
         pool.submit(() -> {
