407ab4243d6af02dda6c8ccf4e4a73fc7a28431f
liushengxi13689209566 <liushengxi13689209566@gmail.com>
2022-05-29 21:40:44 +0800

    add  code

--- algorithm/src/main/java/Temp.java
+++ algorithm/src/main/java/Temp.java
@@ -4,6 +4,24 @@ import java.util.*;
 
 
 public class Temp {
-
+    class Solution {
+        public int[] findOrder(int numCourses, int[][] prerequisites) {
+            int[] result = new int[numCourses];
+            for (int i = 0; i < numCourses; i++) {
+                result[i] = i;
+            }
+            if (prerequisites == null || prerequisites.length <= 0) {
+                return result;
+            }
+            for (int[] prerequisite : prerequisites) {
+                if (prerequisite[0] < prerequisite[1]) {
+                    int tmp = result[prerequisite[0]];
+                    result[prerequisite[0]] = result[prerequisite[1]];
+                    result[prerequisite[1]] = tmp;
+                }
+            }
+            return result;
+        }
+    }
 }
 
--- algorithm/src/main/java/dp/minimumTotal.java
+++ algorithm/src/main/java/dp/minimumTotal.java
@@ -5,11 +5,34 @@ import java.util.List;
 /**
  * @Date: 2022-03-05 23:31
  * @Author: liushengxi
- * @Description:
+ * 题目链接：https://leetcode.cn/problems/IlPe0q/
+ * @Description: 剑指 Offer II 100. 三角形中最小路径之和
+ * 给定一个三角形 triangle ，找出自顶向下的最小路径和。
+ * 每一步只能移动到下一行中相邻的结点上。相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
+ * 也就是说，如果正位于当前行的下标 i ，那么下一步可以移动到下一行的下标 i 或 i + 1 。
+ * 示例 1：
+ * 输入：triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+ * 输出：11
+ * 解释：如下面简图所示：
+ * 2
+ * 3 4
+ * 6 5 7
+ * 4 1 8 3
+ * 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+ * 示例 2：
+ * 输入：triangle = [[-10]]
+ * 输出：-10
+ * 提示：
+ * <p>
+ * 1 <= triangle.length <= 200
+ * triangle[0].length == 1
+ * triangle[i].length == triangle[i - 1].length + 1
+ * -104 <= triangle[i][j] <= 104
+ * 进阶：
+ * 你可以只使用 O(n) 的额外空间（n 为三角形的总行数）来解决这个问题吗？
+ * 注意：本题与主站 120 题相同： https://leetcode-cn.com/problems/triangle/¬
  */
 public class minimumTotal {
-
-
     class Solution {
         public int minimumTotal(List<List<Integer>> triangle) {
             /**
@@ -35,6 +58,4 @@ public class minimumTotal {
             return triangle.get(0).get(0);
         }
     }
-
-
 }
