7e817fb1069c0943c71ab3298a3dbdc941495e33
liushengxi <shengxi.liu@okg.com>
2022-08-28 01:31:35 +0800

    feat: 面试题 17.10. 主要元素

--- algorithm/src/main/java/Temp.java
+++ algorithm/src/main/java/Temp.java
@@ -2,51 +2,38 @@ import com.google.common.collect.Lists;
 
 import java.util.*;
 
-/**
- * 剑指 Offer 51. 数组中的逆序对
- * 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。
- * 示例 1:
- * 输入: [7,5,6,4]
- * 输出: 5
- * 限制：
- * 0 <= 数组长度 <= 50000
- * 思路：
- * （1）暴力解法肯定会超时
- * "horse"
- * "ros"
- * "pale"
- * "ple"
- * "pales"
- * "pal"
- * "inten tion"
- * "execu tion"
- */
 public class Temp {
     static class Solution {
-        public int massage(int[] nums) {
-            if (nums == null || nums.length < 1) {
-                return 0;
+        // 遍历下一个数字时，若它与之前保存的数字相同，则次数加1，否则次数减1；
+        // 若次数为0，则保存下一个数字，并将次数置为1。遍历结束后，所保存的数字即为所求。最后再判断它是否符合条件。
+        public int majorityElement(int[] nums) {
+            if (nums == null) return -1;
+
+            int value = -1;
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                // 若当前无人生还，跟踪下一位士兵的势力
+                if (count == 0) {
+                    value = nums[i];
+                }
+                // 下一位士兵是自己人，那么幸存人数++
+                if (value == nums[i]) count++;
+                // 来了个敌人，同归于尽，幸存人数--
+                else count--;
             }
-            int len = nums.length;
-            // dp[i][2] i 表示到 i 处所获得的最优, i的范围是: 0->n
-            // 另外两个维度表示：0：接  || 1：不接
-            int[][] maxPro = new int[len][2];
-            int result = nums[0];
-            maxPro[0][0] = nums[0];
-            maxPro[0][1] = 0;
-            for (int i = 1; i < len; i++) {
-                // 当天接受预约 = 前一天 不接受预约 的最大值（因为其前一天一定没有被预约）
-                maxPro[i][0] = maxPro[i - 1][1] + nums[i];
-                // 当天不接受预约 = 前一天接受/不接受 的最大值
-                maxPro[i][1] = Math.max(maxPro[i - 1][0], maxPro[i - 1][1]);
+            count = 0;
+            for (int num : nums) {
+                if (num == value) {
+                    count++;
+                }
             }
-            return Math.max(maxPro[len - 1][0], maxPro[len - 1][1]);
+            return count > nums.length / 2 ? value : -1;
         }
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        solution.subSort(new int[]{1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19});
+        solution.majorityElement(new int[]{3, 3, 4});
 //        solution.subSort(new int[]{1, 3, 9, 7, 5});
     }
 }
---
+++ algorithm/src/main/java/set/canPermutePalindrome.java
@@ -0,0 +1,42 @@
+package set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+面试题 01.04. 回文排列
+
+给定一个字符串，编写一个函数判定其是否为某个回文串的排列之一。
+
+回文串是指正反两个方向都一样的单词或短语。排列是指字母的重新排列。
+
+回文串不一定是字典当中的单词。
+
+
+
+示例1：
+
+输入："tactcoa"
+输出：true（排列有"tacocat"、"atcocta"，等等）
+
+思路：
+* */
+public class canPermutePalindrome {
+    class Solution {
+        public boolean canPermutePalindrome(String s) {
+            if (s == null) {
+                return false;
+            }
+            char[] charArray = s.toCharArray();
+            Set<Character> characterSet = new HashSet<>();
+            for (char ch : charArray) {
+                if (characterSet.contains(ch)) {
+                    characterSet.remove(ch);
+                } else {
+                    characterSet.add(ch);
+                }
+            }
+            return characterSet.size() <= 1;
+        }
+    }
+}
